a1c47039e96741a889dc54096141d1ebf9e30042
DASH <luodx04551@163.com>
2021-01-28 19:20:55 +0800

    fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,24 @@
 module.exports = {
-  title: 'Hello VuePress',
-  description: 'Just playing around',
+  title: '天罗地网',
+  description: '天下',
   themeConfig: {
     logo: '/assets/img/logo.png',
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Guide', link: '/guide/' },
-      { text: '测试', link: '/test/' },
+      {
+        text: '更多', link: '/test/', items: [
+          { text: '测试1', link: '/test/01' },
+          { text: '测试2', link: '/test/02' },
+          { text: '关于我们', link: '/test/03' }
+        ]
+      },
       { text: 'Gitee', link: 'https://gitee.com/snares' },
     ],
-    sidebar: [
-      '/',
-      '/page-a',
-      ['/page-b', 'Explicit link text']
-    ]
+    // sidebar: [
+    //   '/',
+    //   '/page-a',
+    //   ['/page-b', 'Explicit link text']
+    // ]
   }
 }
