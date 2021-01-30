7060802eeac728d5d877fc4eb43894cefb1b7a84
DASH <luodx04551@163.com>
2021-01-31 02:04:20 +0800

    fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,18 +2,39 @@ module.exports = {
   title: '天罗地网',
   description: '天下',
   themeConfig: {
+    lastUpdated: 'Last Updated',
     logo: '/assets/img/logo.png',
+    // Git
+    repo: 'https://gitee.com/snares/docs',
+    repoLabel: 'Gitee',
+    docsDir: 'docs',
+    editLinks: true,
+    editLinkText: '帮助我们改善此页面！',
+    smoothScroll: true,
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Guide', link: '/guide/' },
       {
-        text: '更多', link: '/test/', items: [
-          { text: '测试1', link: '/test/01' },
-          { text: '测试2', link: '/test/02' },
-          { text: '关于我们', link: '/test/03' }
+        text: 'SpringCloud', link: '/SpringCloud/', items: [
+          { text: '系统安装', link: '/SpringCloud/01' },
+          { text: '常用命令', link: '/SpringCloud/command' },
+          { text: '关于我们', link: '/SpringCloud/03' }
         ]
       },
-      { text: 'Gitee', link: 'https://gitee.com/snares' },
+      {
+        text: 'Linux', link: '/linux/', items: [
+          { text: '系统安装', link: '/linux/01' },
+          { text: '常用命令', link: '/linux/command' },
+          { text: '关于我们', link: '/linux/03' }
+        ]
+      },
+      {
+        text: '更多', link: '/more/', items: [
+          { text: '测试1', link: '/more/01' },
+          { text: '测试2', link: '/more/02' },
+          { text: '关于我们', link: '/more/03' }
+        ]
+      },
     ],
     // sidebar: [
     //   '/',
